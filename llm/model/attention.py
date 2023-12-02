2dea38cdb2930982a36d45d4fb57e854093bc841
Pe4enkazAMI <yavmaksimov@edu.hse.ru>
2023-12-02 23:02:34 +0300

    fix

--- llm/model/attention.py
+++ llm/model/attention.py
@@ -46,8 +46,7 @@ class MultiHeadAttention(nn.Module):
             score = score / (q.shape[-1]**(0.5))
             if mask is not None:
                 padding_mask = padding_mask.float().masked_fill(padding_mask == 1, float('-inf')).masked_fill(padding_mask == 0, float(0.0))
-                print(padding_mask.shape)
-                score = score + mask + padding_mask.expand(-1, score.shape[1], score.shape[2])
+                score = score + mask + padding_mask.unsqueeze(-1)
             score = F.softmax(score, dim=-1)
             value = score @ v
 
